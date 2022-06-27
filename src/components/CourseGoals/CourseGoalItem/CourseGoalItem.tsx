deefb3e808d90f65e525af2c1bc6966edaac75b2
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-06-27 14:40:43 +0200

    Made ready for starting course

---
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.tsx
@@ -0,0 +1,16 @@
+import './CourseGoalItem.css';
+
+const CourseGoalItem = (props: any) => {
+
+    const deleteHandler = () => {
+        props.onDelete(props.id);
+    };
+
+    return (
+        <li className="goal-item" onClick={deleteHandler}>
+            {props.children}
+        </li>
+    );
+};
+
+export default CourseGoalItem;
